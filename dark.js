8d48abf25ba41317a6c0c2285f04c37a3794aa2f
EX1MKS <blufinder34@gmail.com>
2025-07-06 17:14:35 +0800

    test

---
+++ dark.js
@@ -0,0 +1,5 @@
+ let toggle = document.getElementById("dark-toggle");
+        toggle.addEventListener('click',() => {
+            document.documentElement.classList.toggle('dark')
+            document.documentElement.classList.contains('dark')? toggle.innerText = "☀️" : toggle.innerText = "🌙"
+        })
